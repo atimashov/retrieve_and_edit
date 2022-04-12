52317f960487be785e2fa15e247fc40f3ccc0bf2
Sasha <aleks.k.tim@gmail.com>
2022-04-12 18:29:10 +0800

    env vars

--- editor_code/copy_editor/data.py
+++ editor_code/copy_editor/data.py
@@ -4,9 +4,16 @@ from gtd.io import Workspace
 
 # Set location of local data directory from environment variable
 env_var = 'COPY_EDIT_DATA'
+# if env_var not in os.environ:
+#     assert False, env_var + ' environmental variable must be set.'
+# root = os.environ[env_var]
+
+# Sasha
 if env_var not in os.environ:
-    assert False, env_var + ' environmental variable must be set.'
-root = os.environ[env_var]
+    root = 'data'
+else:
+    root = os.environ[env_var]
+
 
 # define workspace
 workspace = Workspace(root)
